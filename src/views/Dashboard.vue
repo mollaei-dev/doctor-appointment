99b28171c210c2d74de02570f8f9ffa748a13069
fzh.Mollaei <fe.mollaei@gmail.com>
2025-12-02 10:07:10 +0330

    feat: connect dashboard to appointment store and appointment list

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,38 +1,58 @@
 <script setup>
+import AppointmentList from '@/components/AppointmentList.vue'
+import { useAppointmentStore } from '@/stores/appointmentStore'
 import { useUserStore } from '@/stores/userStore'
-import { computed } from 'vue'
+import { computed, watchEffect } from 'vue'
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const userStore = useUserStore()
-const user = computed(() => userStore.currentUser)
+const appointmentStore = useAppointmentStore()
 
 onMounted(() => {
-  const saveUser = localStorage.getItem('currentUser')
+  const saveUser = JSON.parse(localStorage.getItem('currentUser') || '')
   if (saveUser) {
     userStore.login(saveUser)
+    appointmentStore.loadAppointments(saveUser)
   }
 })
+watchEffect(() => {
+  appointmentStore.loadAppointments(userStore.currentUser)
+})
 
+const user = computed(() => userStore.currentUser)
+
+function removeTime(time) {
+  appointmentStore.cancel(time)
+}
 function logout() {
   localStorage.removeItem('currentUser')
   userStore.logOut()
   router.push('login')
 }
-</script> 
+</script>
 <template>
   <div v-if="user" class="dashboard">
     <h1 class="dashboard__title">
       Welcome , <span class="dashboard__email">{{ user.username }}</span>
     </h1>
-    <p class="dashboard__text">This is your dashboard</p>
-    <p class="dashboard__text">No turn has been recorded❗</p>
-    <ul>
-      <li></li>
-    </ul>
-    <button class="dashboard__logout" @click="logout">Booking</button>
-    <button class="dashboard__logout" @click="logout">Log Out</button>
+    <p v-if="appointmentStore.appointments.length === 0" class="dashboard__text">
+      No turn has been recorded❗
+    </p>
+    <div v-else>
+      <p class="dashboard__text">Your booked appointments :</p>
+      <AppointmentList
+        :listItems="appointmentStore.appointments"
+        actionLabel="Cancel"
+        @itemAction="removeTime"
+      />
+    </div>
+
+    <div class="wrapper-button">
+      <button class="dashboard__button" @click="router.push('appointments')">Appointments</button>
+      <button class="dashboard__button" @click="logout">Log Out</button>
+    </div>
   </div>
   <div v-else class="dashboard">
     <p class="dashboard__alert">you are not logged in</p>
@@ -40,6 +60,9 @@ function logout() {
   </div>
 </template>
 <style scoped>
+* {
+  color: #fff;
+}
 .dashboard {
   display: flex;
   flex-direction: column;
@@ -48,14 +71,10 @@ function logout() {
   gap: 30px;
 }
 .dashboard__title {
-  font-size: 22px;
+  font-size: 26px;
 }
 .dashboard__email {
   color: orange;
-}
-
-.dashboard__text {
-  font-size: 25px;
 }
 .dashboard__alert {
   font-size: 30px;
@@ -66,24 +85,33 @@ function logout() {
   font-weight: bold;
   color: orange;
 }
-.dashboard__logout {
+.dashboard__text {
+  font-size: 22px;
+  margin-bottom: 20px;
+}
+.wrapper-button {
+  display: flex;
+  justify-content: space-between;
+  gap: 32px;
+}
+.dashboard__button {
+  width: 170px;
   color: #000;
   background-color: #fff;
   border-radius: 4px;
   text-align: center;
-  padding: 15px 0;
+  padding: 10px;
   display: block;
-  font-size: 25px;
-  width: 100%;
-  margin-top: 46px;
+  font-size: 18px;
+  margin-top: 30px;
   font-weight: bolder;
   transition: all 200ms;
   font-family: inherit;
   border: none;
   outline: none;
-}
-.dashboard__logout:hover {
   cursor: pointer;
+}
+.dashboard__button:hover {
   background-color: rgb(240, 223, 192);
 }
 </style>

d59c46197fac4ac18e6cb2a7c2b1fe31be63d5d0
fzh.Mollaei <fe.mollaei@gmail.com>
2026-01-12 10:59:16 +0330

    fix: define appointmentStore in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useUserStore } from './stores/userStore'
+import { useAppointmentStore } from './stores/appointmentStore'
 
 import App from './App.vue'
 import router from './router'
@@ -10,6 +11,7 @@ const app = createApp(App)
 app.use(createPinia())
 
 const userStore = useUserStore()
+const appointmentStore = useAppointmentStore()
 const savedUser = JSON.parse(localStorage.getItem('currentUser') || 'null')
 if (savedUser) {
   userStore.login(savedUser)
